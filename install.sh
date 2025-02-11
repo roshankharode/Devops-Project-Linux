7be134217a85b6f5f4e040d5453833b62447c2de
roshankharode <roshankharode2000@gmail.com>
2025-02-11 22:59:36 +0530

    2nd veersion

--- install.sh
+++ install.sh
@@ -10,6 +10,9 @@ sudo apt-get install docker.io -y
 sudo apt update -y
 sudo apt install fontconfig openjdk-17-jre -y
 
-sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
-  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-   
+wget -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt-get update
+sudo apt install -y jenkins
+
