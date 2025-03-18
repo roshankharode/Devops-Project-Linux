39a2b7b4f6d701b4c6a6b14f2f498e5ffc54825f
roshankharode <99000074+roshankharode@users.noreply.github.com>
2025-03-18 19:24:18 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,4 +15,4 @@ OpenJDK 64-Bit Server VM (build 17.0.13+11-Debian-2, mixed mode, sharing)
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \https://pkg.jenkins.io/debian-stable binary/ | sudo tee \/etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
-sudo apt-get install jenkins
+sudo apt-get install jenkins -y
