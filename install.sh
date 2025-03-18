712f7b3c2cb1f0358ed51e59d8ebb54dcd4c5881
roshankharode <99000074+roshankharode@users.noreply.github.com>
2025-03-18 19:26:55 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,14 @@
-
 #!/bin/bash
 
-#Updating server
+# Updating server
 sudo apt-get update
 
-#installing java version
-sudo apt install fontconfig openjdk-17-jre
+# Installing Java version
+sudo apt install fontconfig openjdk-17-jre -y
 java -version
-openjdk version "17.0.13" 2024-10-15
-OpenJDK Runtime Environment (build 17.0.13+11-Debian-2)
-OpenJDK 64-Bit Server VM (build 17.0.13+11-Debian-2, mixed mode, sharing)
 
-#Installing Jenkins in server
-sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \https://pkg.jenkins.io/debian-stable binary/ | sudo tee \/etc/apt/sources.list.d/jenkins.list > /dev/null
+# Installing Jenkins on the server
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
 sudo apt-get install jenkins -y
