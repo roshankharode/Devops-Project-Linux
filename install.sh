593ddb565676b6cc9d11be9c1a89ae8efb56f2fb
roshankharode <99000074+roshankharode@users.noreply.github.com>
2025-03-18 18:58:48 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,21 @@
+
 #!/bin/bash
 
-echo "Running belwow Script"
-
+#Updating server
 sudo apt-get update
 
-echo "Installing Docker"
-sudo apt-get install docker.io -y
+#installing java version
+sudo apt install fontconfig openjdk-17-jre
+java -version
+openjdk version "17.0.13" 2024-10-15
+OpenJDK Runtime Environment (build 17.0.13+11-Debian-2)
+OpenJDK 64-Bit Server VM (build 17.0.13+11-Debian-2, mixed mode, sharing)
 
-sudo apt update -y
-sudo apt install fontconfig openjdk-17-jre -y
-
-wget -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-
+#Installing Jenkins in server
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
-sudo apt install -y jenkins
-
+sudo apt-get install jenkins
